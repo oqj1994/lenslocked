6e59b3f43e75b83bbb7072e8d4c444abe17eda26
jia <carryonb@163.com>
2023-11-22 15:00:31 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	message := "call me on 131600231 :)"
+	fmt.Fprint(w, message)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "This is a greate web application")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, r.URL.RawPath)
+}
+
+func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		switch request.URL.Path {
+		case "/":
+			index(writer, request)
+			return
+		case "/contact":
+			contact(writer, request)
+
+			return
+		default:
+			func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusNotFound)
+			}(writer, request)
+			return
+		}
+	})
+
+	fmt.Println("run server on port 10010\nPlease try to enjoy coding!!:)")
+	err := http.ListenAndServe(":10010", nil)
+	if err != nil {
+		panic("run server error!")
+	}
+}
